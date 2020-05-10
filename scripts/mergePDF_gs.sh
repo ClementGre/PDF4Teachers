be97638cc6610f0c0010a8ad168f36ee2ec12452
grensv <vincent.grennerat@univ-grenoble-alpes.fr>
2020-05-10 21:15:39 +0200

    merge script for unix/linux is now interractive

--- scripts/mergePDF_gs.sh
+++ scripts/mergePDF_gs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-FILES='ls *.pdf'
-LIST=${FILES:2}
-gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=merged.pdf -dBATCH $LIST
+echo "fusionner tous les PDF de ce dossier, par ordre alphabétique ?"
+read -p "Taper le nom du fichier de fusion, ou q pour quitter :" ans
+
+if   [ $ans = 'Q' ] || [ $ans = 'q' ]
+then
+	echo "bye"
+else
+	FILES='ls *.pdf'
+	LIST=${FILES:2}
+	gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=$ans -dBATCH $LIST
+fi
