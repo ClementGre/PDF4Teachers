06349178d656d63aea7a38cf1f1b05c45ce1e153
grensv <vincent.grennerat@univ-grenoble-alpes.fr>
2020-05-10 21:14:18 +0200

    rremoved merge.bat

--- scripts/mergePDF.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ------------------------------------------
-echo Fusion des PDF d'un dossier en pdf
-echo ------------------------------------------
-echo -
-
-FOR %%Z in (*.pdf) DO (
-	IF NOT %%Z==merged.pdf IF NOT %%Z==merged2.pdf call mergePDF2.bat %%Z
-)
