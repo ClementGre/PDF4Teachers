b947073e56a10476831f45474dcc728e81f7c133
grensv <vincent.grennerat@univ-grenoble-alpes.fr>
2020-05-18 11:09:45 +0200

    Converts indifferently png & jpg

--- scripts/img2pdf_lot.bat
+++ scripts/img2pdf_lot.bat
@@ -2,11 +2,12 @@
 echo ------------------------------------------
 echo Fusion des images des sous dossiers en pdf
 echo ------------------------------------------
-echo -
-echo erreur normale si pas de fichier jpg dans dossier principal :
+echo - v2020-05-18
+echo erreurs normales si pas de fichier png dans dossier :
 FOR /R %%G in (.) DO (
 	FOR /F "delims=|" %%A IN ("%%G") DO (
-		magick %%~nxA\*.jp*g -resize "2000000@>" %%~nxA.pdf
-		echo ecriture %%~nxA.pdf
+		magick "%%~nxA\*.png" -auto-orient -resize "2000000@>" "%%~nxA.pdf"
+		magick "%%~nxA\*.jp*g" -auto-orient -resize "2000000@>" "%%~nxA.pdf"
+		echo ecriture "%%~nxA.pdf"
 	)
 )
