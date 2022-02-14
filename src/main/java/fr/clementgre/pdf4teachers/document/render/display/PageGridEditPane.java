12d45e9cf192a6ab2a02c9067a0d11820f4275a3
Clément GRENNERAT <clement.grennerat@free.fr>
2022-02-14 16:32:42 +0100

    Update copyright rule
    Took 21 minutes

--- src/main/java/fr/clementgre/pdf4teachers/document/render/display/PageGridEditPane.java
+++ src/main/java/fr/clementgre/pdf4teachers/document/render/display/PageGridEditPane.java
@@ -1,9 +1,13 @@
+/*
+ * Copyright (c) 2022. Clément Grennerat
+ * All rights reserved. You must refer to the licence Apache 2.
+ */
+
 package fr.clementgre.pdf4teachers.document.render.display;
 
 import javafx.scene.layout.Pane;
 
 public class PageGridEditPane extends Pane {
-    
     
     private final PageRenderer page;
     public PageGridEditPane(PageRenderer page){
